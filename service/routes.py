346a5d93b306cf87838cc92661836c1365c53297
Emmanuel Divine <123701270+Deevynn@users.noreply.github.com>
2025-06-10 21:30:00 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,6 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """List all Accounts"""
@@ -83,6 +84,7 @@ def get_accounts(account_id):
     """
     Reads an Account
     """
+    
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -94,6 +96,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """Update an Account"""
